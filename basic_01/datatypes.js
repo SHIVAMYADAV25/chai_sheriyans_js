6c69436399e5c0234e0fa99a56ecb5c0ce84ea6a
shivam yadav <shivamy121314@gmail.com>
2025-10-05 21:07:07 +0000

    5) datatyes complete with ECMA standard

---
+++ basic_01/datatypes.js
@@ -0,0 +1,34 @@
+"use strict"  // explain about that the code is written are new updated javascript
+// treat all the code on newer version of js
+
+//alert( 3 + 4 ) // gives error because in node the alert is written in different way 
+// this will work on browser bc it use the browser v8 engine not the node
+
+
+// to docs mostlused tc39 and mdn
+
+
+//datatypes
+
+let str = "hello"  //string
+let num = 2 // number
+
+// number 2 ** 52 is the capacity what an normal let or const can hold
+// bignit vo hold large number value used in large infrastructure(stock)
+// symbol mostly use in figma and react
+// string = ""
+// null => name = null empty ness of value like for eg: working with API of temperature the value API send is 0 
+// but 0 it self is and temperature value can't show that can us ||(or) operator to change the value to null
+
+// undefined => undefined when variable ids declared but not given and value
+
+//boolean => true or flase
+
+
+//typeof
+//use to find the type of an variable
+// num = 2 typeof num ==> number
+// str = "string" str ==> string
+
+//unique
+//when using the typeof null it return object and tyepof undefined => undefined
