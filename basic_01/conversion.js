458e6d1e3214a7c4f9e93ef48e9967ea93de059a
shivam yadav <shivamy121314@gmail.com>
2025-10-22 11:38:49 +0000

    datatype conversion confusion

---
+++ basic_01/conversion.js
@@ -0,0 +1,70 @@
+let score = true;
+
+//let {scroce} = req.body;  // dont know what going to come
+
+
+//let score = 100 
+//console.log(typeof score);  //number
+//console.log(score)  //100
+
+
+//let score = "100";
+//console.log(typeof score);  string
+//console.log(score) 100
+
+
+// let score = "100abc";
+// score = Number(score)
+// console.log(typeof score);  //number
+// console.log(score) //NaN
+
+//let score = "NULL";
+//score = Number(score)
+//console.log(typeof score);  //if converting in number it would be number and if not converting it would be object
+//console.log(score) // if converted it would be 0 or else it would be null
+
+
+// let score = "undefined";
+// score = Number(score)
+// console.log(typeof score);  //number
+// console.log(score) //NaN
+
+// let score = "true";
+// console.log(typeof score) //boolean
+// score = Number(score)
+// console.log(typeof score);  //number
+// console.log(score) //1
+
+
+// "33"=> 33
+// "33abc:" => NaN
+//true = 1 and false = 0
+
+
+
+
+//boolean
+let isLoggedIn = 1;
+let check = Boolean(isLoggedIn)
+//console.log(check); //true
+//console.log(typeof check); //boolean
+
+
+// "" => false 
+//"shivam" => true
+// 1 => true
+//0 => false
+
+
+//string
+
+let num = 55;
+console.log(typeof num); //number
+let constring = String(num)
+console.log(typeof constring); //number
+console.log(constring); //55
+
+// Instring the converstion is done mostly seen using the typeof
+
+
+
