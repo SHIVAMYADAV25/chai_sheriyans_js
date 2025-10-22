ff92cf95d61e55dd2bf0636607cb237cb2949b4d
shivam yadav <shivamy121314@gmail.com>
2025-10-22 12:06:54 +0000

    Why string to number conversion is confusing

--- basic_01/conversion.js
+++ basic_01/conversion.js
@@ -59,12 +59,68 @@ let check = Boolean(isLoggedIn)
 //string
 
 let num = 55;
-console.log(typeof num); //number
+//console.log(typeof num); //number
 let constring = String(num)
-console.log(typeof constring); //number
-console.log(constring); //55
+//console.log(typeof constring); //number
+//console.log(constring); //55
 
 // Instring the converstion is done mostly seen using the typeof
 
+//******************************************operation on string **************************************************************
 
+// let value = 3;
+// console.log(-value); // -3
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2*2);
+
+
+// let str1 = "shivam"
+// let str2 = " yadav"
+
+// console.log(str1 + str2);  // concadination 
+// o/p shivam yadav
+
+// console.log("1" + 2); 12
+// console.log(1 + "2"); 12
+// console.log("1" + 2 + 2); 122
+// console.log(1 + 2 + "2"); 32
+//            ^
+//            | 
+// operation precedense in js is done by the left to right herarchy
+
+
+// this are the bad way of writting the code 
+// console.log(+true); // 1
+// console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let counter = 100;
+counter++
+console.log(counter) //101 (increment by 1)
+
+// study at
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+// there are so many conversion like 
+// tobigint
+// tostring 
+// tonumber 
+// toobject 
+// tolength 
+// toindex 
+// ordinarytoprimitive 
+// toboolean 
+// tointeger and more
+//  everything conversion has different o/p depending on the input type provided  
 
