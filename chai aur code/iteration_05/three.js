24f2c1e0b07161924aaf56accb568ba772abf64b
shivam yadav <shivamy121314@gmail.com>
2025-11-08 06:38:24 +0000

    for-of for-in and map()

---
+++ chai aur code/iteration_05/three.js
@@ -0,0 +1,73 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+// o/p:
+// 1
+// 2
+// 3
+// 4
+// 5
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`)
+}
+
+// o/p:
+// Each char is H
+// Each char is e
+// Each char is l
+// Each char is l
+// Each char is o
+// Each char is  
+// Each char is w
+// Each char is o
+// Each char is r
+// Each char is l
+// Each char is d
+// Each char is !
+
+// Maps
+// has unique value 
+// store data kinda form in object
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+// Map(3) {
+//   'IN' => 'India',
+//   'USA' => 'United States of America',
+//   'Fr' => 'France'
+// }
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+// IN :- India
+// USA :- United States of America
+// Fr :- France
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// }
+
+// o/p:
+// TypeError: myObject is not iterable
