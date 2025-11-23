a0077ecf523a05d0b1685761f76521638997deeb
shivam yadav <shivamy121314@gmail.com>
2025-11-23 04:15:06 +0000

    class and super() => (call(this,username)

---
+++ chai aur code/Object_class_oops/myClasses.js
@@ -0,0 +1,46 @@
+// ES6
+
+class User {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+
+}
+
+//constructor is called when new keywords is written do make 
+// check function/variable -> give value to this keywords -> set the context -> then run
+const chai = new User("chai", "chai@gmail.com", "123")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+// this is what happend behind the scene
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
